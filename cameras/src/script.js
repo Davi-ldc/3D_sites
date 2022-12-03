acd231d25bd2bb6b30c4fd03a3b4c2b64533b8d3
Davi-ldc <96698943+Davi-ldc@users.noreply.github.com>
2022-12-03 11:11:40 -0300

    👀👀

--- cameras/src/script.js
+++ cameras/src/script.js
@@ -1,5 +1,6 @@
 import './style.css'
 import * as THREE from 'three'
+import { MapControls, OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
 
 
@@ -35,47 +36,52 @@ const scene = new THREE.Scene()
 const mesh = new THREE.Mesh(
     new THREE.BoxGeometry(1, 1, 1, 5, 5, 5),
     new THREE.MeshBasicMaterial({ color: 0xff0000 })
-)
-scene.add(mesh)
-
-// Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-//  basicamente e o qua perto q vc ta da cena, proporção da tela (largura/altura)
-//o quao perto vc consegue ver (se for mt alto vai sempre dar a impressao dq vc ta dentro do cubo)
-//o quao lonje vc consegue ver 
-
-// const aspectRadio = sizes.width / sizes.height
-// const camera = new THREE.OrthographicCamera(
-//     -1 * aspectRadio, 
-//     1 * aspectRadio,
-//     1 * aspectRadio,
-//     -1 * aspectRadio,
-//     0.1,
-//     100
-//     )
-
-/*
-a ideia da OrthographicCamera é vc definir o tamnho do randerer (lembrando q ele tem forma quadrada)
-ai cada um dos 4 primeiros parametros é uma das linhas (esquerda, direita, cima e baixo respectivamente)
-e os ultimos dois sao o msm do ultimos 2 do de cima
-o problema e q dependendo do tamnho do canvas pd ser q o rander fique estranho pq ai agnt ta usando o msm tamnho pra
-uma tv de cinema e meu pc. pra resolver isso se multiplica os valores por sizes.width / sizes.height
-no geral e mlhr usar a PerspectiveCamera
-*/
-
-
-
-
-camera.position.z = 3
-camera.lookAt(mesh.position)
-scene.add(camera)
-
-// Renderer
-const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
-})
-renderer.setSize(sizes.width, sizes.height)
-
+    )
+    scene.add(mesh)
+    
+    // Camera
+    const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+    //  basicamente e o qua perto q vc ta da cena, proporção da tela (largura/altura)
+    //o quao perto vc consegue ver (se for mt alto vai sempre dar a impressao dq vc ta dentro do cubo)
+    //o quao lonje vc consegue ver 
+    
+    // const aspectRadio = sizes.width / sizes.height
+    // const camera = new THREE.OrthographicCamera(
+    //     -1 * aspectRadio, 
+    //     1 * aspectRadio,
+    //     1 * aspectRadio,
+    //     -1 * aspectRadio,
+    //     0.1,
+    //     100
+    //     )
+    
+    /*
+    a ideia da OrthographicCamera é vc definir o tamnho do randerer (lembrando q ele tem forma quadrada)
+    ai cada um dos 4 primeiros parametros é uma das linhas (esquerda, direita, cima e baixo respectivamente)
+    e os ultimos dois sao o msm do ultimos 2 do de cima
+    o problema e q dependendo do tamnho do canvas pd ser q o rander fique estranho pq ai agnt ta usando o msm tamnho pra
+    uma tv de cinema e meu pc. pra resolver isso se multiplica os valores por sizes.width / sizes.height
+    no geral e mlhr usar a PerspectiveCamera
+    */
+   
+   
+   
+   
+   camera.position.z = 3
+   camera.lookAt(mesh.position)
+   scene.add(camera)
+   
+   // Renderer
+   const renderer = new THREE.WebGLRenderer({
+       canvas: canvas
+    })
+    renderer.setSize(sizes.width, sizes.height)
+    
+//Controls
+// a ideia do OrbitControls é que ele vai atualizar a camera automaticamente
+const Crontrols = new OrbitControls(camera, canvas)//ele recebe a camera e o canvas 
+Crontrols.enableDamping = true
+Crontrols.dampingFactor = 0.05 //controla a intencidade do damping padrão = 0.05
 // Animate
 const clock = new THREE.Clock()
 
@@ -83,13 +89,18 @@ const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
 
+    Crontrols.update()
+    //então o efeito de Damping faz com que quando eu solte o mause ele ainda continue mechendo divagarinho
+    //ate parar. pra ele frz isso precisa ser atualizado a cada frame.
+    
     // Update objects
-    mesh.rotation.y = elapsedTime;
+    //mesh.rotation.y = elapsedTime;
 
-    camera.position.x = Math.sin(cursor.x * Math.PI *2) * 2 // pq sin e cos retorna valores entre 0 e 1 ai a camera fica
-    camera.position.z = Math.cos(cursor.x * Math.PI *2) * 2//mt perto do cubo
-    camera.position.y = cursor.y *5
-    camera.lookAt(mesh.position)
+    // camera.position.x = Math.sin(cursor.x * Math.PI *2) * 2 // o vezes 2 é pq sin e cos retorna valores entre 0 e 1 ai a camera fica
+    // camera.position.z = Math.cos(cursor.x * Math.PI *2) * 2//mt perto do cubo
+    //cm eu quero q ele complete uma volta inteira eu to passando pi*2
+    // camera.position.y = cursor.y *5
+    // camera.lookAt(mesh.position)
     // Render
     renderer.render(scene, camera)
 
